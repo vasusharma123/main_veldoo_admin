5db755546c888adec8fc96d3f748d670fa245b50
Ankush Garg <ankushgarg20251@gmail.com>
2022-11-30 20:44:50 +0530

    Home page setup based on new structure

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="public/css/landing-pg-style.css" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/landing-pg-style.css')}}" rel="stylesheet" type="text/css">
   </head>
   <body>
 	<div class="hay-ln-pg-main">
@@ -12,17 +12,17 @@
 				<div class="hay-ln-pg-main-row">
 					<div class="hay-ln-pg-main-left">
 						<div class="hay-ln-pg-main-left-cont">
-							<div class="hay-ln-pg-main-logo"><img src="public/images/logo.png" /></div>
+							<div class="hay-ln-pg-main-logo"><img src="{{ asset('images/logo.png')}}" /></div>
 							<h1>Our app is now on Android and iPhone</h1>
 							<ul>
-								<li><a href="#"><img src="public/images/glp-button.png" /></a></li>
-								<li><a href="#"><img src="public/images/aps-button.png" /></a></li>
+								<li><a href="#"><img src="{{ asset('images/glp-button.png')}}" /></a></li>
+								<li><a href="#"><img src="{{ asset('images/aps-button.png')}}" /></a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="hay-ln-pg-main-right">
 						<div class="hay-ln-pg-main-right-img">
-							<img src="public/images/phone-image.png" />
+							<img src="{{ asset('images/phone-image.png')}}" />
 						</div>
 					</div>
 				</div>
