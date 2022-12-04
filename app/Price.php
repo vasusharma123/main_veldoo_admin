2114bf8b9f68120ae46bc283b2b3de4e141de0d8
Ankush Garg <ankushgarg20251@gmail.com>
2022-12-04 22:44:31 +0530

    add base url with car type images

--- app/Price.php
+++ app/Price.php
@@ -8,44 +8,49 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Price extends Model
 {
-    use SoftDeletes;
-    
-    protected $fillable = [
- 		'car_type',
- 		'car_image',
- 		'price_per_km',
- 		'basic_fee',
- 		'seating_capacity',
- 		'alert_time',
- 		'status'];
-     /**
+  use SoftDeletes;
 
-     * The attributes that should be mutated to dates.
+  protected $fillable = [
+    'car_type',
+    'car_image',
+    'price_per_km',
+    'basic_fee',
+    'seating_capacity',
+    'alert_time',
+    'status'
+  ];
 
-     *
 
-     * @var array
+  protected $dates = ['deleted_at'];
 
-     */
+  public $timestamps = true;
+  protected $guarded = [];
 
-    protected $dates = ['deleted_at'];
+  protected $hidden = ["deleted_at"];
 
-       public $timestamps = true;
-    protected $guarded = [];
-    
-    protected $hidden = ["deleted_at"];
-  		
+  protected $appends = [
+		'image_with_url'
+	];
 
- public static function deleteData($id){
- 	return self::where('id',$id)->delete();
- }
-function carType(){
-		return $this->hasOne(\App\Category::class,'id','car_type');
+  public static function deleteData($id)
+  {
+    return self::where('id', $id)->delete();
+  }
+  function carType()
+  {
+    return $this->hasOne(\App\Category::class, 'id', 'car_type');
+  }
+
+  public function cars()
+  {
+    return $this->hasMany(Vehicle::class, 'category_id', 'id');
+  }
+
+  public function getImageWithUrlAttribute(){
+		if(!empty($this->car_image)){
+			return env('URL_PUBLIC').'/'.$this->car_image;
+		}
+		return $this->image;
 	}
 
-	public function cars()
-    {
-      return $this->hasMany(Vehicle::class,'category_id','id');
-    }
-	
 }
