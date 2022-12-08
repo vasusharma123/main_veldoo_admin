c5f969272f16fd95a353ff7347ad9fe4b80471d0
Ankush Garg <ankushgarg20251@gmail.com>
2022-12-08 21:12:33 +0530

    expense image url addon for API

--- app/ExpenseAttachment.php
+++ app/ExpenseAttachment.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class ExpenseAttachment extends Model
 {
     protected $fillable = ['expense_id', 'url'];
+
+    protected $appends = [
+		'image_with_url'
+	];
+
+    public function getImageWithUrlAttribute(){
+		if(!empty($this->url)){
+			return env('URL_PUBLIC').'/'.$this->url;
+		}
+		return $this->image;
+	}
 }
