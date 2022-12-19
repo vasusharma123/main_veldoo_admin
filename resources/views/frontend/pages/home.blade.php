a9c1a964ee36484042e784d9132d7896fba7a88f
Ankush Garg <ankushgarg20251@gmail.com>
2022-12-19 08:29:00 -0600

    Website images changes

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -12,17 +12,17 @@
 				<div class="hay-ln-pg-main-row">
 					<div class="hay-ln-pg-main-left">
 						<div class="hay-ln-pg-main-left-cont">
-							<div class="hay-ln-pg-main-logo"><img src="{{ asset('images/logo.png')}}" /></div>
+							<div class="hay-ln-pg-main-logo"><img src="{{ asset('images/vel_web_logo.png')}}" /></div>
 							<h1>Our app is now on Android and iPhone</h1>
 							<ul>
-								<li><a href="#"><img src="{{ asset('images/glp-button.png')}}" /></a></li>
-								<li><a href="#"><img src="{{ asset('images/aps-button.png')}}" /></a></li>
+								<li><a href="https://play.google.com/store/apps/details?id=com.dev.veldoouser"><img src="{{ asset('images/glp-button.png')}}" /></a></li>
+								<li><a href="https://apps.apple.com/in/app/id1597936025"><img src="{{ asset('images/aps-button.png')}}" /></a></li>
 							</ul>
 						</div>
 					</div>
 					<div class="hay-ln-pg-main-right">
 						<div class="hay-ln-pg-main-right-img">
-							<img src="{{ asset('images/phone-image.png')}}" />
+							<img src="{{ asset('images/phone_group.png')}}" />
 						</div>
 					</div>
 				</div>
