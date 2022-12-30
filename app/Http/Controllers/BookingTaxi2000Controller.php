6928eb6435d7b555c5cb039a500de191fd0ec60e
Ankush Garg <ankushgarg20251@gmail.com>
2022-12-30 17:38:55 +0530

    image append in driver data in notiification

--- app/Http/Controllers/BookingTaxi2000Controller.php
+++ app/Http/Controllers/BookingTaxi2000Controller.php
@@ -585,7 +585,7 @@ if($_REQUEST['cm'] == 2)
 				// 	$ride->cancel_reason = $request->cancel_reason;
 				// }
 				$ride->save();
-				$ride_detail = Ride::select('id', 'accept_time', 'note', 'pick_lat', 'pick_lng', 'pickup_address', 'dest_address', 'dest_lat', 'dest_lng', 'distance', 'driver_id', 'passanger', 'ride_cost', 'ride_time', 'ride_type', 'waiting', 'status', 'user_id', 'driver_id')->with(['user:id,first_name,last_name,country_code,phone,current_lat,current_lng', 'driver:id,first_name,last_name,country_code,phone,current_lat,current_lng'])->find($ride_id);
+				$ride_detail = Ride::select('id', 'accept_time', 'note', 'pick_lat', 'pick_lng', 'pickup_address', 'dest_address', 'dest_lat', 'dest_lng', 'distance', 'driver_id', 'passanger', 'ride_cost', 'ride_time', 'ride_type', 'waiting', 'status', 'user_id', 'driver_id')->with(['user:id,first_name,last_name,country_code,phone,current_lat,current_lng,image', 'driver:id,first_name,last_name,country_code,phone,current_lat,current_lng,image'])->find($ride_id);
 
 				$settings = Setting::first();
 				$settingValue = json_decode($settings['value']);
