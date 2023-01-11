35b7cc177bb92c5eed7572cad6bf6d8f58e48bda
Ankush Garg <ankushgarg20251@gmail.com>
2023-01-11 08:13:12 +0530

    Error Log Problems When Using Laravel Passport for User Login Authentication

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,6 +34,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+        if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException && $exception->getCode() == 9) {
+            return;
+        }
+        
         parent::report($exception);
     }
 
