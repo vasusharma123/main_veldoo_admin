8bca651a7783a1749e9a6aed5bea1060da7268f7
Ankush Garg <ankushgarg20251@gmail.com>
2023-02-23 23:18:18 +0530

    Last unseen api issue

--- app/Console/Commands/DriverActiveStateFirstReminder.php
+++ app/Console/Commands/DriverActiveStateFirstReminder.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use App\DriverStayActiveNotification;
 use App\Setting;
 use App\User;
+use App\Notification;
 
 class DriverActiveStateFirstReminder extends Command
 {
@@ -61,6 +62,10 @@ class DriverActiveStateFirstReminder extends Command
                 bulk_firebase_android_notification($title, $message, $android_driver_tokens, $additional);
             }
             DriverStayActiveNotification::whereIn('driver_id', $unactive_driver_ids)->update(['is_availability_alert_sent' => 1, 'last_activity_time' => Carbon::now()]);
+            foreach ($unactive_driver_ids as $driverid) {
+                $notification_data[] = ['title' => $title, 'description' => $message, 'type' => 12, 'user_id' => $driverid, 'additional_data' => json_encode($additional), 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()];
+            }
+            Notification::insert($notification_data);
         }
     }
 }
