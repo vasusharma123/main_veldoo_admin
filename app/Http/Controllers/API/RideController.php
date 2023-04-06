b93c173d3d6d09383f9728b8588bf4df264e32e2
Ankush Garg <ankushgarg20251@gmail.com>
2023-04-07 00:13:39 +0530

    cancel ride new API created

--- app/Http/Controllers/API/RideController.php
+++ app/Http/Controllers/API/RideController.php
@@ -1665,4 +1665,80 @@ class RideController extends Controller
         }
     }
 
+    public function cancel_ride(Request $request)
+    {
+        $logged_in_user = Auth::user();
+        $rules = [
+            'status' => 'required',
+            'ride_id' => 'required',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json(['message' => trans('api.required_data'), 'error' => $validator->errors()], $this->warningCode);
+        }
+        try {
+            $ride = Ride::find($request->ride_id);
+
+            if (!empty($ride)) {
+                $userdata = User::find($ride['user_id']);
+                $deviceToken = $userdata['device_token'] ?? "";
+                $deviceType = $userdata['device_type'] ?? "";
+                if ($request->status == -2) {
+                    if ($ride['status'] == -2) {
+                        return response()->json(['success' => true, 'message' => "Ride Cancelled already"], $this->successCode);
+                    }
+                    if (!empty($request->delete_for_all) && $request->delete_for_all == 1 && !empty($ride->parent_ride_id)) {
+                        Ride::where(['parent_ride_id' => $ride->parent_ride_id])->where('ride_time', '>', Carbon::now())->update(['note' => $request->note ?? "", 'status' => '-2', 'updated_at' => Carbon::now()]);
+                    } else {
+                        $rideObj = Ride::find($request->ride_id);
+                        $rideObj->note = $request->note ?? "";
+                        $rideObj->status = -2;
+                        $rideObj->save();
+                    }
+
+
+                    if (!empty($userdata)) {
+                        $ride_detail = Ride::select('id', 'note', 'pick_lat', 'pick_lng', 'pickup_address', 'dest_address', 'dest_lat', 'dest_lng', 'distance', 'passanger', 'ride_cost', 'ride_time', 'ride_type', 'waiting', 'status', 'user_id', 'driver_id', 'payment_type', 'alert_time', 'car_type', 'company_id', 'vehicle_id', 'parent_ride_id')->with(['user:id,first_name,last_name,country_code,phone,current_lat,current_lng,image', 'driver:id,first_name,last_name,country_code,phone,current_lat,current_lng,image', 'company_data:id,name,logo,state,city,street,zip,country', 'car_data:id,model,vehicle_image,vehicle_number_plate,category_id', 'car_data.carType:id,car_type,car_image', 'vehicle_category:id,car_type,car_image'])->find($request->ride_id);
+                        $settings = \App\Setting::first();
+                        $settingValue = json_decode($settings['value']);
+                        $ride['waiting_time'] = $settingValue->waiting_time;
+                        if (!empty($ride_detail->driver)) {
+                            $ride_detail->driver->car_data = $ride_detail->driver->car_data;
+                            $ride_detail->driver->avg_rating = $ride_detail->getAvgRating($ride_detail->driver->id);
+                        }
+                        $title = 'Ride Cancelled';
+                        $message = "Ride Cancelled Successfully";
+                        $type = 5;
+                        $additional = ['type' => $type, 'ride_id' => $ride->id, 'ride_data' => $ride_detail];
+                        if (!empty($deviceToken)) {
+                            if ($deviceType == 'android') {
+                                bulk_firebase_android_notification($title, $message, [$deviceToken], $additional);
+                            }
+                            if ($deviceType == 'ios') {
+                                bulk_pushok_ios_notification($title, $message, [$deviceToken], $additional, $sound = 'default', $userdata['user_type']);
+                            }
+                        }
+
+                        $notification = new Notification();
+                        $notification->title = $title;
+                        $notification->description = $message;
+                        $notification->type = $type;
+                        $notification->user_id = $userdata['id'];
+                        $notification->additional_data = json_encode($additional);
+                        $notification->save();
+                    }
+                }
+            } else {
+                return response()->json(['success' => false, 'message' => "No such ride exist"], $this->warningCode);
+            }
+            return response()->json(['success' => true, 'message' => $message, 'data' => $ride_detail], $this->successCode);
+        } catch (\Illuminate\Database\QueryException $exception) {
+            Log::info($exception->getMessage() . "--" . $exception->getLine());
+            return response()->json(['success' => false, 'message' => $exception->getMessage()], $this->warningCode);
+        } catch (\Exception $exception) {
+            Log::info($exception->getMessage() . "--" . $exception->getLine());
+            return response()->json(['success' => false, 'message' => $exception->getMessage()], $this->warningCode);
+        }
+    }
 }
