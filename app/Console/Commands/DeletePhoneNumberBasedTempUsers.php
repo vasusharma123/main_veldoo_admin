d850f9e2dc0454f3a48b7dec19558cc345a716f2
Ankush Garg <ankushgarg20251@gmail.com>
2023-06-17 21:19:55 +0530

    Cronjob changes related to delete user

--- app/Console/Commands/DeletePhoneNumberBasedTempUsers.php
+++ app/Console/Commands/DeletePhoneNumberBasedTempUsers.php
@@ -45,18 +45,17 @@ class DeletePhoneNumberBasedTempUsers extends Command
         $settingValue = json_decode($settings['value']);
         $defaultDays = $settingValue->temporary_phone_number_users??40;
         $deletedTime = Carbon::now()->subDays($defaultDays)->format('Y-m-d');
-        User::where(['user_type' => 1, 'app_installed' => 0])->where(function($query){
+        User::join('users as creator','creator.id','=','users.created_by')->where(['users.user_type' => 1, 'users.app_installed' => 0])->where(function($query){
             $query->where(function($query1){
-                $query1->whereNull('first_name')->orWhere(['first_name' => '']);
+                $query1->whereNull('users.first_name')->orWhere(['users.first_name' => '']);
             })->where(function($query1){
-                $query1->whereNull('last_name')->orWhere(['last_name' => '']);
+                $query1->whereNull('users.last_name')->orWhere(['users.last_name' => '']);
             })->where(function($query1){
-                $query1->whereNull('email')->orWhere(['email' => '']);
+                $query1->whereNull('users.email')->orWhere(['users.email' => '']);
             })->where(function($query1){
-                $query1->whereNotNull('phone')->where('phone', '!=', '');
+                $query1->whereNotNull('users.phone')->where('users.phone', '!=', '');
             });
-        })->whereHas('creator', function ($query) {
-            $query->where('user_type', 2);
-        })->whereDate('created_at', '<', $deletedTime)->forceDelete();
+        })->where(['creator.user_type' => 2])
+        ->whereDate('users.created_at', '<', $deletedTime)->forceDelete();
     }
 }
