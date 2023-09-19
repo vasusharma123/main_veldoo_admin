76763069e6e20eff726c67662df667e297e9e94b
Ankush Garg <ankushgarg20251@gmail.com>
2023-09-19 21:24:45 +0530

    enable inspect on company-login and remove extra code from privacy policy

--- resources/views/layouts/company.blade.php
+++ resources/views/layouts/company.blade.php
@@ -15,7 +15,7 @@
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="https://dunggramer.github.io/disable-devtool/disable-devtool.min.js" defer></script>
+        {{-- <script src="https://dunggramer.github.io/disable-devtool/disable-devtool.min.js" defer></script> --}}
         <!-- Custom Js -->
         <script src="{{ asset('new-design-company/assets/js/main.js') }}" type="application/javascript"></script>
         @yield('script')
