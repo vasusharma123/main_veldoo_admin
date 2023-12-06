767c4afa104bf6dab0fadc1c7a5c4bf58edeab7b
Ankush Garg <ankushgarg20251@gmail.com>
2023-12-06 07:07:49 +0530

    Master admin seeder changes

--- database/seeds/MasterAdminSeeder.php
+++ database/seeds/MasterAdminSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class MasterAdminSeeder extends Seeder
 {
@@ -12,8 +14,9 @@ class MasterAdminSeeder extends Seeder
     public function run()
     {
         $data = [
-            [   'name'  => 'master' , 'first_name' => 'ma', 'last_name' => 'admin', 'country_code' => 91, 'phone' => 65635267, 'country_code_iso' => 'in', 'user_type' => 6,'email' => 'aca.pavlovic@veldoo.com','password'=> '$2y$10$xSf2IjFe1lBCZACwvh06FeH1dP33C.XrkGLnrvHpY5EqwOTrbeiBe', 'status'=>1, 'verify' => 1 ]        ];
-        
-        DB::table('users')->insert($data);
+            ['name'  => 'master', 'first_name' => 'ma', 'last_name' => 'admin', 'country_code' => 41, 'phone' => 65635267, 'country_code_iso' => 'ch', 'user_type' => 6, 'email' => 'aca.pavlovic@veldoo.com', 'password' => Hash::make('xT@J,G!<?%]z'), 'status' => 1, 'verify' => 1]
+        ];
+
+        User::insert($data);
     }
 }
