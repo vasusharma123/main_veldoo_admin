b0a9aab378d6fc21d0f9c38744d716d3419047e9
Maniraj singh <maniraj.singh@webners.com>
2024-01-09 19:10:16 +0530

    added service_provider_id check on getVoucher

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6216,14 +6216,25 @@ print_r($data['results'][0]['geometry']['location']['lng']); */
 	}
 	public function getVouchers(Request $request)
 	{
+
+		$rules = [
+			'service_provider_id' => 'required',
+		];
+
+		$validator = Validator::make($request->all(), $rules);
+		if ($validator->fails()) {
+			return response()->json(['message' => trans('api.required_data'), 'error' => $validator->errors()], $this->warningCode);
+		}
+
 		$user = Auth::user();
 		$user_id = $user['id'];
+		$service_provider_id = $request->service_provider_id;
 		
 
 		try {
-			$vouchers = UserVoucher::with('service_provider')->where([['user_id', '=', $user_id]])->where('service_provider_id',$user['service_provider_id'])->orderBy('id', 'desc')->get()->toArray();
-			$plus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '!=', 3]])->where('service_provider_id',$user['service_provider_id'])->sum('miles');
-			$minus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '=', 3]])->where('service_provider_id',$user['service_provider_id'])->sum('miles');
+			$vouchers = UserVoucher::with('service_provider')->where([['user_id', '=', $user_id]])->where('service_provider_id',$service_provider_id)->orderBy('id', 'desc')->get()->toArray();
+			$plus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '!=', 3]])->where('service_provider_id',$service_provider_id)->sum('miles');
+			$minus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '=', 3]])->where('service_provider_id',$service_provider_id)->sum('miles');
 
 			$total_miles = $plus_vouchers - $minus_vouchers;
 
