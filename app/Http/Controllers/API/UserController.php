2d112c4c05799e713f193116cab3d8a8800a1f46
Maniraj singh <maniraj.singh@webners.com>
2024-01-10 12:58:15 +0530

    updated get voucher

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6217,24 +6217,25 @@ print_r($data['results'][0]['geometry']['location']['lng']); */
 	public function getVouchers(Request $request)
 	{
 
-		$rules = [
-			'service_provider_id' => 'required',
-		];
-
-		$validator = Validator::make($request->all(), $rules);
-		if ($validator->fails()) {
-			return response()->json(['message' => trans('api.required_data'), 'error' => $validator->errors()], $this->warningCode);
-		}
 
 		$user = Auth::user();
 		$user_id = $user['id'];
-		$service_provider_id = $request->service_provider_id;
+
+		
 		
 
 		try {
-			$vouchers = UserVoucher::with('service_provider')->where([['user_id', '=', $user_id]])->where('service_provider_id',$service_provider_id)->orderBy('id', 'desc')->get()->toArray();
-			$plus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '!=', 3]])->where('service_provider_id',$service_provider_id)->sum('miles');
-			$minus_vouchers = UserVoucher::where([['user_id', '=', $user_id], ['type', '=', 3]])->where('service_provider_id',$service_provider_id)->sum('miles');
+			$vouchers_qry = UserVoucher::with('service_provider')->where([['user_id', '=', $user_id]]);
+			$plus_vouchers_qry = UserVoucher::where([['user_id', '=', $user_id], ['type', '!=', 3]]);
+			$minus_vouchers_qry = UserVoucher::where([['user_id', '=', $user_id], ['type', '=', 3]]);
+			if($request->service_provider_id){
+				$vouchers_qry->where('service_provider_id',$request->service_provider_id);
+				$plus_vouchers_qry->where('service_provider_id',$request->service_provider_id);
+				$minus_vouchers_qry->where('service_provider_id',$request->service_provider_id);
+			}
+			$vouchers = $vouchers_qry->orderBy('id', 'desc')->get()->toArray();
+			$plus_vouchers = $plus_vouchers_qry->sum('miles');
+			$minus_vouchers = $minus_vouchers_qry->sum('miles');
 
 			$total_miles = $plus_vouchers - $minus_vouchers;
 
